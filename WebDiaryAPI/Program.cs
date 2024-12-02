4c45884b9f9c0904062df99149b99fc362c22c88
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2024-12-02 15:17:21 +0100

    Setting up a Swagger

--- WebDiaryAPI/Program.cs
+++ WebDiaryAPI/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.OpenApi.Models;
+
 namespace WebDiaryAPI
 {
     public class Program
@@ -11,15 +13,37 @@ namespace WebDiaryAPI
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            //builder.Services.AddOpenApi();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "My API",
+                    Version = "v1",
+                    Description = "API for managing resources",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Your Name",
+                        Email = "your.email@example.com"
+                    }
+                });
+            });
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+                    options.RoutePrefix = string.Empty; // Ustawienie, aby Swagger by³ dostêpny na stronie g³ównej
+                });
             }
+            
 
             app.UseHttpsRedirection();
 
