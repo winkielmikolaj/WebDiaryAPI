69b1d44625c893db516f49359ee1140dc20ece28
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2024-12-04 18:34:17 +0100

    Updating(PUT Endpoint)

--- WebDiaryAPI/Controllers/DiaryEntriesController.cs
+++ WebDiaryAPI/Controllers/DiaryEntriesController.cs
@@ -50,9 +50,50 @@ namespace WebDiaryAPI.Controllers
             await _context.SaveChangesAsync();
 
             //cos z lokalizacja
-            var resourceUrl = Url.Action(nameof(GetDiaryEntries), new {id=diaryEntry.Id});
+            var resourceUrl = Url.Action(nameof(GetDiaryEntries), new { id = diaryEntry.Id });
 
             return Created("resourceUrl", diaryEntry);
         }
+
+        //updating = put
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDiaryEntry(int id, [FromBody] DiaryEntry diaryEntry)
+        {
+            if (id != diaryEntry.Id)
+            {
+                //400 response
+                return BadRequest();
+            }
+
+            _context.Entry(diaryEntry).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (DiaryEntryExists(id))
+                {
+                    //404 response notfound
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            //204 response
+            return NoContent();
+        }
+
+
+        private bool DiaryEntryExists(int id)
+        {
+            return _context.DiaryEntries.Any(e => e.Id == id);
+        }
+
     }
 }
+
+//[FromBody] parameter binding
